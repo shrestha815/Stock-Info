b652486b479c3960b47f693b134d22a6d7e9225b
Safal Shrestha <sshrestha24@wooster.edu>
2025-11-11 18:00:15 -0500

    Initial Setup

---
+++ stocks-backend/src/test/java/com/safal/stockinfo/stocks_backend/StocksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safal.stockinfo.stocks_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
